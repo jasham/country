6ec13a65b8662aebbc5b03723ea30630cc4cfbe6
Jasham <jasham009@gmail.com>
2022-09-18 19:09:58 +0530

    updating code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,13 +2,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 const Document = () => {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap"
           rel="stylesheet"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="./images/favicon-32x32.png"
         />
       </Head>
       <body className="font-nunito bg-whiteDMLMElem dark:bg-darkBlueDMElem min-h-screen">
